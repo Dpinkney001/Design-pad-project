65e411fb5fe31d796dccaefcbd65811ea7def749
Duvall Pinkney <14938639+Dpinkney001@users.noreply.github.com>
2021-11-06 12:06:07 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import DesignContainer from 'C:/Users/thees/OneDrive/Desktop/design-pad/src/components/DesignContainer.js';
+import PadInput from 'C:/Users/thees/OneDrive/Desktop/design-pad/src/components/PadInput.js';
+import './App.css';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <DesignContainer />
+      <PadInput />
+
+
+    </div>
+  );
+}
+
+export default App;
